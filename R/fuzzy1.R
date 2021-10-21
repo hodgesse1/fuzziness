3e157d69ab1022e7e490da47dff423fe75435be7
hodgesse1 <erinm1960@outlook.com>
2021-10-21 00:29:45 -0600

    update documentation

--- R/fuzzy1.R
+++ R/fuzzy1.R
@@ -1,3 +1,8 @@
+#' adding normal variables to a vector
+#' @param an input vector/number
+#' @return the input \code{x} + \code{rnorm}
+#' @examples
+#'  fuzzy1(1:5)
 fuzzy1 <- function(x) {
   return(x+rnorm(length(x)))
 }
